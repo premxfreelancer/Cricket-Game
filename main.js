c3c828da01259154769c50c84c3ef815f2dded26
Prem <premruidas123@gmail.com>
2024-04-26 13:52:16 +0530

    v1

---
+++ main.js
@@ -0,0 +1,7 @@
+var dotball = document.getElementById('sounddotball');
+  var six = document.getElementById('soundsix');
+  var four = document.getElementById('soundfour');
+  var double = document.getElementById('sounddouble');
+  var single = document.getElementById('soundsingle');
+  var wicket = document.getElementById('soundwicket');
+  
